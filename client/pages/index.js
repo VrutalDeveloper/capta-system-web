d3a455aa9adce9a461578c4c982bc3d38f747a95
Jack Sari <janasarii@gmail.com>
2021-09-12 19:50:15 -0500

    add firebase client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,36 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import db from "../config/firebase";
 
 
 export default function Home() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect( () => {
+
+    (async () => {
+      const querySnapshot = await getDocs(collection(db, "posts"));
+
+      const posts = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      setPosts(posts)
+    })()
+ 
+  }, [])
+
+
   return (
      <div className="bg-red-300">
+
+        {
+          posts.map((post) => {
+            return <div key={post.id} >{post.title}</div>
+          })
+        }
+
        <h1>Hola</h1>
      </div>
   )
